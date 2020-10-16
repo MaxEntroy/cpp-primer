9af8a68b55f8a1671d3f71797d2f2262f526c735
MaxEntroy <likang@tju.edu.cn>
2020-10-16 11:25:47 +0800

    chapter13: Add demo-07 in sec-01.

---
+++ chapter13/sec-01/demo-07/base.h
@@ -0,0 +1,21 @@
+#ifndef BASE_H_
+#define BASE_H_
+
+#include <iostream>
+
+class Base {
+ public:
+  Base() = default;
+
+  Base(const Base&) = delete;
+  Base& operator=(const Base&) = delete;
+
+  virtual ~Base() = default;
+
+  virtual void Print() {
+    std::cout << "Base::Print() called." << std::endl;
+  }
+
+};
+
+#endif
